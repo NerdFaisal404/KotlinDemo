0f5b7facc0171c210343a96664e2451998dc876c
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2017-05-20 11:26:37 +0600

    ConditionalWhen added

---
+++ src/conditionals/ConditionMode.kt
@@ -0,0 +1,20 @@
+package conditionals
+
+/**
+ * Created by Faisal Ahmed on 5/20/2017.
+ */
+
+fun main(args: Array<String>) {
+    val mode: Int = 5;
+
+    when(mode){
+        1 -> println("This mode is lazy faisal mode")
+        2 ->{
+            println("This mode is 2.")
+            println("So the mode is busy.")
+        }
+
+        3 -> println("This mode is super productive")
+        else -> println("I don't know this mode.")
+    }
+}
---
+++ src/conditionals/Conditionals.kt
@@ -0,0 +1,14 @@
+package conditionals
+
+/**
+ * Created by Faisal Ahmed on 5/20/2017.
+ */
+
+fun main(args: Array<String>) {
+    val age: Int = 25
+    if (age < 18) {
+        print("boyos hoi nai ;) ")
+    } else {
+        print("biye dei na ken :( ")
+    }
+}
